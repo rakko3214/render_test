539d8954cdd49a0c458dddf1870bd8a91db55712
河本行平 <me32140941@gmail.com>
2025-01-21 17:54:33 +0900

    lintチェック修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
     skip_before_action :require_login, only: %i[new create]
-  
+
     def new
       @user = User.new
     end
-  
+
     def create
       @user = User.new(user_params)
       if @user.save
@@ -13,15 +13,14 @@ class UsersController < ApplicationController
         render :new
       end
     end
-  
+
     def show
       @name = current_user.name
     end
-  
+
     private
-  
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  end
-  
+end
