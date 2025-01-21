7a9ad7bddec2d4de6113a22ed307d2486ff07b43
河本行平 <me32140941@gmail.com>
2025-01-21 16:35:26 +0900

    ログイン機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "tops#index"
+  resources :users, only: %i[new create show]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
